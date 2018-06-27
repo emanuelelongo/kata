2b87e016a7a02811cf83b88a24af40af8a7d10aa
Emanuele Longo <emanuele.longo@ynap.com>
2018-06-27 10:29:56 +0200

    removed dictionary from ValidNumber improving performance

--- cs/ValidNumber/Solution.cs
+++ cs/ValidNumber/Solution.cs
@@ -5,90 +5,81 @@ namespace ValidNumber
 {
     public class Solution
     {
-        public enum State {Space1, Space2, Sign1, Sign2, Dot1, Dot2, Scient, Num1, Num2, Num3, NotValid}
+        public enum State {Space1, Space2, Sign1, Sign2, Dot1, Dot2, Scient, Num1, Num2, Num3}
 
-        Dictionary<State, Func<char, State>> machine;
-
-        public Solution()
-        {
-            machine = new Dictionary<State, Func<char, State>>();
-            
-            machine[State.Space1] = c =>
-            {
-                if(c == ' ') return State.Space1;
-                if(c == '+' || c == '-') return State.Sign1;
-                if(c == '.') return State.Dot1;
-                if(char.IsDigit(c)) return State.Num1;
-                return State.NotValid;
-            };
-
-            machine[State.Sign1] = c => {
-                if(c == '.') return State.Dot1;
-                if(char.IsDigit(c)) return State.Num1;
-                return State.NotValid;
-            };
-
-            machine[State.Dot1] = c => {
-                if(char.IsDigit(c)) return State.Num2;
-                return State.NotValid;
-            };
-
-            machine[State.Num1] = c => {
-                if(char.IsDigit(c)) return State.Num1;
-                if(c == '.') return State.Dot2;
-                if(c == 'e') return State.Scient;
-                if(c == ' ') return State.Space2;
-                return State.NotValid;
-            };
-
-            machine[State.Dot2] = c => {
-                if(c == ' ') return State.Space2;
-                if(c == 'e') return State.Scient;
-                if(char.IsDigit(c)) return State.Num2;
-                return State.NotValid;
-            };
-
-            machine[State.Space2] = c => {
-                if(c == ' ') return State.Space2;
-                return State.NotValid;
-            };
-
-            machine[State.Num2] = c => {
-                if(char.IsDigit(c)) return State.Num2;
-                if(c == 'e') return State.Scient;
-                if(c == ' ') return State.Space2;
-                return State.NotValid;
-            };
-
-            machine[State.Scient] = c => {
-                if(char.IsDigit(c)) return State.Num3;
-                if(c == '+' || c == '-') return State.Sign2;
-                return State.NotValid;
-            };
-
-            machine[State.Sign2] = c => {
-                if(char.IsDigit(c)) return State.Num3;
-                return State.NotValid;
-            };
-
-            machine[State.Num3] = c => {
-                if(char.IsDigit(c)) return State.Num3;
-                if(c == ' ') return State.Space2;
-                return State.NotValid;
-            };
-         }
-
-        public bool IsNumber(string s)
+        public bool IsNumber(string s)  
         {
             State state = State.Space1;
             int i = 0;
+            char c;
             while(i < s.Length) 
             {
-                state = machine[state](s[i]);
-                
-                if(state == State.NotValid)
-                    return false;
+                c = s[i];
+                switch(state) 
+                {
+                    case State.Space1:
+                        if(c == ' ') state = State.Space1;
+                        else if(c == '+' || c == '-') state = State.Sign1;
+                        else if(c == '.') state = State.Dot1;
+                        else if(char.IsDigit(c)) state = State.Num1;
+                        else return false;
+                        break;
 
+                    case State.Sign1:
+                        if(c == '.') state = State.Dot1;
+                        else if(char.IsDigit(c)) state = State.Num1;
+                        else return false;
+                        break;
+
+                    case State.Dot1:
+                        if(char.IsDigit(c)) state = State.Num2;
+                        else return false;
+                        break;
+
+                    case State.Num1:
+                        if(char.IsDigit(c)) state = State.Num1;
+                        else if(c == '.') state = State.Dot2;
+                        else if(c == 'e') state = State.Scient;
+                        else if(c == ' ') state = State.Space2;
+                        else return false;
+                        break;
+
+                    case State.Dot2:
+                        if(c == ' ') state = State.Space2;
+                        else if(c == 'e') state = State.Scient;
+                        else if(char.IsDigit(c)) state = State.Num2;
+                        else return false;
+                        break;
+
+                    case State.Space2:
+                        if(c == ' ') state = State.Space2;
+                        else return false;
+                        break;
+
+                    case State.Num2:
+                        if(char.IsDigit(c)) state = State.Num2;
+                        else if(c == 'e') state = State.Scient;
+                        else if(c == ' ') state = State.Space2;
+                        else return false;
+                        break;
+
+                    case State.Scient:
+                        if(char.IsDigit(c)) state = State.Num3;
+                        else if(c == '+' || c == '-') state = State.Sign2;
+                        else return false;
+                        break;
+
+                    case State.Sign2:
+                        if(char.IsDigit(c)) state = State.Num3;
+                        else return false;
+                        break;
+
+                    case State.Num3:
+                        if(char.IsDigit(c)) state = State.Num3;
+                        else if(c == ' ') state = State.Space2;
+                        else return false;
+                        break;
+                }
                 i++;
             }
             if(state == State.Num1 
